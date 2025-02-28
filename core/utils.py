c6e609d4a7649c9560729b0b7e7d5cc1dc0a7c8d
SilianZ <silian_zheng@outlook.com>
2025-02-28 19:22:06 +0800

    fix: 修复漏洞

--- core/utils.py
+++ core/utils.py
@@ -6,9 +6,11 @@ def verify_turnstile_token(token: str) -> bool:
         with httpx.Client() as client:
             response = client.post("https://challenges.cloudflare.com/turnstile/v0/siteverify", json={"secret": secret, "response": token})
             data = response.json()
+            print(data)
             if data["success"]:
                 return True
             else:
                 return False
     except Exception as e:
+        print(e)
         return False
